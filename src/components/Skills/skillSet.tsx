ddeac4d784cd734b1155e0d3440c11d2d405413b
Muneer shafi <zmzahidmuneer@gmail.com>
2025-01-01 20:12:20 +0530

    fixing home page issues

--- src/components/Skills/skillSet.tsx
+++ src/components/Skills/skillSet.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import "./skillSet.css";
-import { FaBootstrap, FaDatabase, FaHtml5, FaJs, FaNodeJs, FaPhp, FaPython, FaReact } from "react-icons/fa6";
+import {
+  FaBootstrap,
+  FaDatabase,
+  FaHtml5,
+  FaJs,
+  FaNodeJs,
+  FaPhp,
+  FaPython,
+  FaReact,
+} from "react-icons/fa6";
 type Props = {};
 
 const SkillBar = ({ name, level, icon, index }) => {
-  const style = {
-    animation: `slideIn 0.5s ease forwards ${index * 0.1}s`,
-    opacity: 0,
-  };
-
   return (
-    <div className="skill-item" style={style}>
+    <div className="skill-item">
       <div className="skill-header">
-        <div className="skill-icon-name">
-          <div className="icon-container">{icon}</div>
-          <span className="skill-name">{name}</span>
-        </div>
+          <div className="cols align-center">
+            {icon}
+            <strong>{name}</strong>
+          </div>
         <span className="skill-level">{level}%</span>
       </div>
       <div className="progress-bar">
@@ -34,7 +38,7 @@ export default function SkillSet({}: Props) {
     { name: "Node.js", level: 80, icon: <FaJs /> },
     { name: "SQL", level: 75, icon: <FaDatabase /> },
   ];
- const frontEndSkills = [
+  const frontEndSkills = [
     { name: "Html/Css", level: 90, icon: <FaHtml5 /> },
     { name: "JavaScript/Typescript", level: 85, icon: <FaJs /> },
     { name: "React", level: 80, icon: <FaReact /> },
@@ -42,18 +46,18 @@ export default function SkillSet({}: Props) {
   ];
 
   return (
-    <div className="cols flex-center">
-      <div className="skills-container">
-        <h2 className="skills-title">Professional  backend Skills</h2>
-        <div className="skills-grid">
+    <div className="grid-container">
+      <div className="rows white-card">
+        <h2 className="cols gap-md">Professional backend Skills</h2>
+        <div className="">
           {backEndSkills.map((skill, index) => (
             <SkillBar key={skill.name} {...skill} index={index} />
           ))}
         </div>
       </div>
-      <div className="skills-container">
-        <h2 className="skills-title">Professional frontend Skills</h2>
-        <div className="skills-grid">
+      <div className="rows white-card">
+        <h2 className="cols gap-md">Professional frontend Skills</h2>
+        <div className="">
           {frontEndSkills.map((skill, index) => (
             <SkillBar key={skill.name} {...skill} index={index} />
           ))}
